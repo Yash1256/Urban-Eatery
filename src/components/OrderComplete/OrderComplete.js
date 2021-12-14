9d29e9877dab685b35541578485f5efd56c69067
codernishchay <nishchay.vns2000@gmaiil.com>
2021-12-14 05:21:43 -0500

    delete order done

--- src/components/OrderComplete/OrderComplete.js
+++ src/components/OrderComplete/OrderComplete.js
@@ -1,18 +1,38 @@
 import React, { useEffect } from 'react';
 import MapImg from '../../images/img/map.png';
+import * as firebase from "firebase/app";
 import Rider from '../../images/img/rider.png';
 import RiderHelmet from '../../images/img/helmet.png';
 import orders from '../../fakeData/delivery';
 
 const OrderComplete = (props) => {
-
+    
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
-    // const orders = find( orders => orders.id === id );
 
     const { flat, road } = props.deliveryDetails;
+    const  {orderID } = props.orderDetails ; 
+    console.log(props); 
+    const ordersRef = firebase.firestore().collection("/user");
 
+    const cancelorder = function(e){
+        e.preventDefault() ; 
+        if(orderID != "" || orderID != null){
+            ordersRef.doc(orderID).delete().then(function(res) {
+                console.log(res)
+                props.setorderDetailsHandler({
+                    deliveryDetails : null, 
+                    orderID : null   
+                })
+                
+            }).catch((err) => {
+              console.log(err) ; 
+            })
+        }else {
+        console.log("order ID was null , cant do any thing")
+    }
+} 
 
     return (
         <div className="container my-5">
@@ -24,6 +44,10 @@ const OrderComplete = (props) => {
                     <div className="bg-light p-3 rounded">
                         <img className="w-25 ml-5" src={Rider} alt="" />
                         <div className="bg-white  rounded p-3 my-3">
+                        <div>
+                                <h5>Your OrderID</h5>
+                                <p>{orderID}</p>
+                            </div>
                             <div>
                                 <h5>Your Location</h5>
                                 <p>{flat}, {road}</p>
@@ -44,8 +68,8 @@ const OrderComplete = (props) => {
                             </div>
                         </div>
 
+                        <button className="btn btn-block my-3 btn-danger" onClick={cancelorder}>Cancel Order</button>
                         <button className="btn btn-block my-3 btn-danger">Contact</button>
-
                     </div>
                 </div>
             </div>
