3934cdb14994e541d9cbdb8013ca59aedb3d42fe
Yash1256 <shuklayash1256@gmail.com>
2021-12-16 20:42:11 +0530

    Final Code

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,49 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import './Blog.css';
-import BlogItem from '../BlogItem/BlogItem';
+import React, { useState, useEffect } from "react";
+import "./Blog.css";
+import BlogItem from "../BlogItem/BlogItem";
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const getData = () => {
+      fetch("blog.json", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then(function (response) {
+          //   console.log(response);
+          return response.json();
+        })
+        .then(function (myJson) {
+          setBlogs(myJson);
+          //   console.log(myJson);
+        });
+    };
+    getData();
+  }, []);
 
-    const [blogs, setBlogs] = useState([]);
-    useEffect(() => {
-        const getData = () => {
-            fetch("blog.json", {
-              headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-              },
-            })
-              .then(function (response) {
-                console.log(response);
-                return response.json();
-              })
-              .then(function (myJson) {
-                setBlogs(myJson);
-                console.log(myJson);
-              });
-          };
-          getData();
-    }, []);
+  return (
+    <section className="features my-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="text-center mt-2 header-text">Why You Chose Us!</h3>
+            <p className="mt-3 mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+              sapiente eaque repellendus asperiores nisi! Architecto,
+              praesentium eligendi consequatur inventore fuga eius totam officia
+              adipisci. Nostrum quia soluta vel distinctio delectus!
+            </p>
+          </div>
+        </div>
 
-    return (
-        <section className='features my-5'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-12'>
-                        <h3 className='text-center mt-2 header-text'>Why You Chose Us!</h3>
-                        <p className='mt-3 mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente eaque repellendus asperiores nisi! Architecto, praesentium eligendi consequatur inventore fuga eius totam officia adipisci. Nostrum quia soluta vel distinctio delectus!
-                        </p>
-                    </div>
-                </div>
-
-                <div className='row'>
-                    {
-                        blogs.map(blog => <BlogItem key={blog.id} blog={blog}></BlogItem>)
-                    }
-                </div>
-            </div>
-        </section>
-    )
-}
+        <div className="row">
+          {blogs.map((blog) => (
+            <BlogItem key={blog.id} blog={blog}></BlogItem>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Blog;
